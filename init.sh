ff94773f441fdb3e53ba9e5efc280670453f6972
bokwoon <bokwoon.c@gmail.com>
2019-04-04 14:46:11 +0800

    update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
-echo "hello! this is https://raw.githubusercontent.com/bokwoon95/dotfiles/master/init.sh"
-bash -c "$(curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/bokwoon95/dotfiles/master/gitconfig.sh)"
-echo "I reached here!"
+init () {
+  echo "hello! this is https://raw.githubusercontent.com/bokwoon95/dotfiles/master/init.sh"
+  bash -c "$(curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/bokwoon95/dotfiles/master/gitconfig.sh)"
+  echo "I reached here!"
+}
+init
