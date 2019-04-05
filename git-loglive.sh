42207a19e4392c5d05089bca31e1318139e0f807
bokwoon <bokwoon.c@gmail.com>
2019-04-05 15:03:55 +0800

    yee

--- git-loglive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-clear
-find .git -type f |
-  entr git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --reflog |
-  grep "(HEAD" --context=10
