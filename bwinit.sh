8dab70244f0ad3be09dcafcfacb045a921d72695
bokwoon <bokwoon.c@gmail.com>
2019-04-15 15:09:34 +0800

    the standalone gitconfig repo is now the single
    source of truth for gitconfig.sh. I don't want to maintain the same file
    accross multiple repositories

--- bwinit.sh
+++ bwinit.sh
@@ -1,6 +1,6 @@
 init () {
   echo "hello! this is https://raw.githubusercontent.com/bokwoon95/dotfiles/master/init.sh"
-  bash -c "$(curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/bokwoon95/dotfiles/master/gitconfig.sh)"
+  bash -c "$(curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/bokwoon95/gitconfig/master/gitconfig.sh)"
   echo "I reached here!"
 }
 init
